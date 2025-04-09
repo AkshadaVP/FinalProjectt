41a814915f642cf62ff65f9ef32f650b0496b868
AkshadaVP <144597702+AkshadaVP@users.noreply.github.com>
2025-04-09 11:31:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 // Get the publishable key from environment variables
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_API_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
